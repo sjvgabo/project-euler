672bd4748a8c9a8242918f225ccd04d6fa81d108
sjvgabo <103029718+sjvgabo@users.noreply.github.com>
2022-06-15 17:01:26 +0800

    Update sorts.py
    removed the trials and created a single function to test the time for each sort

--- algorithms/sorts.py
+++ algorithms/sorts.py
@@ -80,40 +80,20 @@ def merge_sort(lst):
 lst = [1,8, 4, 6, 3, 10, 9]
 print(merge_sort(lst))
 
-lst = [random.randint(1, 10000) for n in range(100000)]
-bubble_lst = lst[:]
-selection_lst = lst[:]
-simple_lst = lst[:]
-insertion_lst = lst[:]
-merge_lst = lst[:]
-#
-# start_bubble = time.time()
-# bubble_sort(bubble_lst)
-# end_bubble = time.time()
-# print('Bubble Sort')
-# print(f'Time: {start_bubble-end_bubble}')
-#
-# start_selection = time.time()
-# bubble_sort(selection_lst)
-# end_selection = time.time()
-# print('Selection Sort')
-# print(f'Time: {start_selection-end_selection}')
-#
-# start_simple = time.time()
-# simple_sort(simple_lst)
-# end_simple = time.time()
-# print('Simple Sort')
-# print(f'Time: {start_simple-end_simple}')
-#
-# start_insertion = time.time()
-# insertion_sort(insertion_lst)
-# end_insertion = time.time()
-# print('Insertion Sort')
-# print(f'Time: {start_insertion-end_insertion}')
+sort_methods = [merge_sort, 
+           insertion_sort, 
+           selection_sort, 
+           simple_sort, 
+           bubble_sort
+          ]
 
-start_merge = time.time()
-merge_sort(merge_lst)
-end_merge = time.time()
-print('Merge Sort')
-print(len(lst))
-print(f'Time: {start_merge-end_merge}')
+def test_speed_all(sort_methods):
+    for sort_method in sort_methods:
+        lst = [random.randint(1, 10000) for _ in range(10000)]
+
+        start = time.time()
+        sort_method(lst)
+        end = time.time()
+        print(f'Sort Method: {sort_method.__name__}')
+        print(f'Time: {end - start}')
+    
